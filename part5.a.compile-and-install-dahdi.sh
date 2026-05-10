e0da88c25f01aa7a33cd807512493c7ac0b4ca85
ashloverscn <ashloverscn@gmail.com>
2026-05-11 01:11:45 +0530

    Create part5.a.compile-and-install-dahdi.sh

---
+++ part5.a.compile-and-install-dahdi.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+#ver=3.1.0
+#oem=1
+ver=3.4.0
+oem=0
+
+echo -e "\e[0;32m Install Dahdi Audio_CODEC Driver v$ver \e[0m"
+sleep 2
+cd /usr/src
+yum install kernel-devel-$(uname -r) -y
+#rm -rf dahdi-linux-complete*
+yum remove dahdi* -y
+yum remove dahdi-tools* -y
+#yum install dahdi* -y
+#yum install dahdi-tools* -y
+if [ $oem -eq 1 ]
+then
+	wget http://download.vicidial.com/required-apps/dahdi-linux-complete-2.3.0.1+2.3.0.tar.gz
+	tar -xvzf dahdi-linux-complete-2.3.0.1+2.3.0.tar.gz
+	cd dahdi-linux-complete-2.3.0.1+2.3.0
+else
+	wget -O dahdi-linux-complete-$ver+$ver.tar.gz https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-$ver+$ver.tar.gz
+	tar -xvzf dahdi-linux-complete-$ver+$ver.tar.gz
+	cd dahdi-linux-complete-$ver+$ver
+fi
+
+#: ${JOBS:=$(( $(nproc) + $(nproc) / 2 ))}
+: ${JOBS:=$(nproc)}
+make -j ${JOBS} all
+make install
+make config
+make install-config
+#yum -y install dahdi-tools-libs
+modprobe dahdi
+modprobe dahdi_dummy
+dahdi_genconf -v
+dahdi_cfg -v
+
+cd tools
+make clean
+make -j ${JOBS} all
+make install
+make install-config
+
+cd /etc/dahdi
+\cp -r system.conf system.conf.bak
+\cp -r system.conf.sample system.conf
+
+echo -e "\e[0;32m Enable dahdi.service in systemctl \e[0m"
+sleep 2
+
+\cp -r /etc/systemd/system/dahdi.service /etc/systemd/system/dahdi.service.bak
+rm -rf /etc/systemd/system/dahdi.service
+touch /etc/systemd/system/dahdi.service
+
+tee /etc/systemd/system/dahdi.service <<'EOF'
+[Unit]
+Description=DAHDI Telephony Drivers
+After=network.target
+Before=asterisk.service
+
+[Service]
+Type=oneshot
+ExecStartPre=/sbin/modprobe dahdi
+ExecStartPre=/sbin/modprobe dahdi_dummy
+ExecStart=/usr/sbin/dahdi_cfg -v
+ExecReload=/usr/sbin/dahdi_cfg -v
+ExecStop=/usr/sbin/dahdi_cfg -v
+Restart=on-failure
+RestartSec=2
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#restart dahdi Service
+systemctl daemon-reload && \
+systemctl disable dahdi.service && \
+systemctl enable dahdi.service && \
+systemctl restart dahdi.service && \
+systemctl status dahdi.service | head -n 18
+
+\cp -r /dahdi.sh /dahdi.sh.bak
+rm -rf /dahdi.sh
+\cp -r  /usr/src/dahdi.sh /dahdi.sh
+
+chmod +x /dahdi.sh 
